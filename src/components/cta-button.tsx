d6f8d4b4d30b161515804355dfa8e820ea272b4b
Vinicius Gouvea <vrgouvea@outlook.com>
2025-07-31 14:52:44 -0300

    fix: z index for CTA changed to avoid miss click on mobile chart

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -23,7 +23,7 @@ export default function FloatingWhatsappButton() {
       href="https://wa.me/5541992478837?text=Olá!%20Entrei%20no%20site%20da%20Brainstorm%20e%20gostaria%20de%20tirar%20algumas%20dúvidas."
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-20 right-6 z-50 transition-colors bg-transparent "
+      className="fixed bottom-20 right-6 z-20 transition-colors bg-transparent "
     >
       <Button variant="whatsapp" size="zp" title="whatsapp button CTA">
         <WhatsApp />
