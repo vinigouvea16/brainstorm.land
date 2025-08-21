e0f64230533f6645b3220076c3dc782271294980
Vinicius Gouvea <vrgouvea@outlook.com>
2025-08-21 16:36:20 -0300

    feat: prismic io revalidade function

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,17 @@
-import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+import { NextResponse } from 'next/server'
+import { revalidateTag } from 'next/cache'
 
-export async function POST() {
-  revalidateTag("prismic");
+export async function POST(request: Request) {
+  const secret = request.headers.get('Prismic-Webhook-Secret')
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  if (secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
+    return NextResponse.json(
+      { message: 'Invalid secret token' },
+      { status: 401 }
+    )
+  }
+
+  revalidateTag('prismic')
+
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
