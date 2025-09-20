1cd980074fa0f904bd411bef55901564c14c3643
Vinicius Gouvea <vrgouvea@outlook.com>
2025-09-20 17:25:02 -0300

    trio special offer price fix

--- src/components/trio-special-offer.tsx
+++ src/components/trio-special-offer.tsx
@@ -5,7 +5,7 @@ export function TrioSpecialOffer() {
     <div className="my-4">
       <div className="bg-brain-span/10 border border-brain-span/30 rounded-lg p-6 text-center">
         <p className="text-lg lg:text-xl font-windsor text-brain-span">
-          Na compra desse kit economize R$ 77,76 (20%)
+          Na compra desse kit economize R$ 47,76 (20%)
         </p>
         <p className="text-sm lg:text-base text-brain-text/70 mt-2">
           comparado à compra individual dos 3 suplementos
